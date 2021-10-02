87e5de176f705002b90e0b9a760a883d57b6eb98
karoterra <karoterra2008@gmail.com>
2021-10-03 00:36:46 +0900

    プロジェクト ファイルを追加します。

---
+++ AupFontExtractor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AupFontExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
